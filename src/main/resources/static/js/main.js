3e76d2b14cbae7de7b6670c0748308e741993114
gessica mariana Goulart <gessica_mariana12@hotmail.com>
2020-10-05 19:58:15 +0100

    Working on EDIT

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,23 +1,41 @@
+$(document).ready(function (){
+    listeners();
+    });
+
 function showProfiles(){
     fetch("http://localhost:8080/profiles")
     .then(response => response.json())
     .then(function(data){
         data.forEach(element => {
             $("<tr><td>" + element.id + "</td>"+
-    "<td>" + element.name + "</td>" + 
+    "<td>" + element.name + "</td>" +
     "<td>" + element.height + "</td>" +
     "<td>" + element.weight + "</td>" +
     "<td>" + element.age +
-                "</td> <td><button type=\"button\" class=\"btn btn-secondary\" onclick=\"window.location.href='/edit'\">Edit</button></td>" +
+                "</td> <td><button type=\"button\" id='editBtn' class=\"btn btn-secondary\" onclick='editForm(" + element.id + ")'>Edit</button></td>" +
                 "<td><button type=\"button\" class=\"btn btn-danger\" onclick='deleteProfile(" + element.id + ")'>Delete</button>" +
                 "</td></tr>").appendTo('#profile-table');
 });
 })
 }
-
+let listeners = function () {
+       if($('#editBtn') !== null) {
+           addEventListener("click", () => window.location.href = '/edit');
+       }
+};
+function editForm(id){
+    console.log('Redirecting to edit page');
+    fetch("http://localhost:8080/profileid/" + id)
+        .then(response => response.json())
+        .then(function(element){
+                $("<tr><td>" + element.id + "</td>"+
+                    "<td>" + element.name + "</td>" +
+                    "<td>" + element.height + "</td>" +
+                    "<td>" + element.weight + "</td>" +
+                    "<td>" + element.age + "</td></tr>").appendTo('#editForm');
+        });
+}
 function updateProfile(){
-    let str = $("#editForm");
-
     const mapToObject = (obj) => {
         let mapped = {};
         obj.serializeArray().forEach((field) => {
@@ -25,18 +43,16 @@ function updateProfile(){
         });
         return mapped;
     };
+    let str = $("#editForm");
     let mapped = mapToObject(str);
-/*    if($('editForm')){
-       return  console.log(JSON.stringify(mapped));
-    }*/
-
+    console.log(mapped);
     $.ajax({
         url: 'http://localhost:8080/update',
         type: 'POST',
         data: JSON.stringify(mapped),
         contentType: 'application/json',
         dataType: 'json',
-        success: window.location.href = '/index',
+        success: console.log(JSON.stringify(mapped)),
         error: (function (xhr, textStatus, errorThrown){
             alert('Error! Status = ' + xhr.status);
         })
@@ -52,8 +68,8 @@ function deleteProfile(id){
         contentType: 'application/json',
         dataType: 'json',
         success: function (){
+            alert('Profile with id number ' + deleted + ' deleted');
             location.reload();
-            alert('Id number ' + deleted + ' deleted');
         }(),
         error: function(jqXHR, textStatus, errorThrown) {
             console.log(jqXHR);
@@ -76,7 +92,7 @@ function deleteProfile(id){
       let values = Object.values(mapped);
 
         if(values.includes('') || values.includes(NaN)){
-            alert('Shiiiiit');
+            alert('Please fill in all the fields before saving a profile!');
         }else {
             $.ajax({
                 url: 'http://localhost:8080/addProfile',
